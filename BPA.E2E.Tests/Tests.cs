0745638466a2ace4217cc63d4882f15adc9e6fff
linhp <linhpqq@gmail.com>
2023-06-22 10:43:03 +0700

    Add project files.

---
+++ BPA.E2E.Tests/Tests.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+// To run functional tests for ASP.NET Web Forms and ASP.NET Core Blazor XAF Applications,
+// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
+//
+// -For Google Chrome: download "chromedriver.exe" from https://chromedriver.chromium.org/downloads.
+// -For Microsoft Edge: download "msedgedriver.exe" from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/.
+//
+// Selenium requires a path to the downloaded driver. Add a folder with the driver to the system's PATH variable.
+//
+// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
+
+namespace BPA.Module.E2E.Tests;
+
+public class BPATests : IDisposable {
+    const string BlazorAppName = "BPABlazor";
+    const string AppDBName = "BPA";
+    EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+
+	public BPATests() {
+        FixtureContext.RegisterApplications(
+            new BlazorApplicationOptions(BlazorAppName, string.Format(@"{0}\..\..\..\..\BPA.Blazor.Server", Environment.CurrentDirectory))
+        );
+        FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "BPAEasyTest", server: @"(localdb)\mssqllocaldb"));	           
+	}
+    public void Dispose() {
+        FixtureContext.CloseRunningApplications();
+    }
+    [Theory]
+    [InlineData(BlazorAppName)]
+    public void TestBlazorApp(string applicationName) {
+        FixtureContext.DropDB(AppDBName);
+        var appContext = FixtureContext.CreateApplicationContext(applicationName);
+        appContext.RunApplication();
+        Assert.True(appContext.Navigate("Reports.Dashboards"));
+        Assert.True(appContext.Navigate("Reports.Reports"));
+        Assert.True(appContext.Navigate("Reports.Audit Event"));
+    }
+}
---
+++ BPA.Module/BusinessObjects/BPADbContext.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+
+namespace BPA.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class BPAContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<BPAEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new BPAEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class BPADesignTimeDbContextFactory : IDesignTimeDbContextFactory<BPAEFCoreDbContext> {
+	public BPAEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<BPAEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=BPA");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new BPAEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(BPAContextInitializer))]
+public class BPAEFCoreDbContext : DbContext {
+	public BPAEFCoreDbContext(DbContextOptions<BPAEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<FileData> FileData { get; set; }
+	public DbSet<ReportDataV2> ReportDataV2 { get; set; }
+	public DbSet<DashboardData> DashboardData { get; set; }
+    public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+    public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReference { get; set; }
+    public DbSet<Event> Event { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.UsePropertyAccessMode(PropertyAccessMode.PreferFieldDuringConstruction);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.AuditItems)
+            .WithOne(p => p.AuditedObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.OldItems)
+            .WithOne(p => p.OldObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.NewItems)
+            .WithOne(p => p.NewObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.UserItems)
+            .WithOne(p => p.UserObject);
+    }
+}
+
+public class BPAAuditingDbContext : DbContext {
+    public BPAAuditingDbContext(DbContextOptions<BPAAuditingDbContext> options) : base(options) {
+    }
+    public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+    public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReference { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.AuditItems)
+            .WithOne(p => p.AuditedObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.OldItems)
+            .WithOne(p => p.OldObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.NewItems)
+            .WithOne(p => p.NewObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.UserItems)
+            .WithOne(p => p.UserObject);
+    }
+}
